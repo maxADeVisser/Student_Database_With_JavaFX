4acfe8647f36b6d1e53fe206061264ef17f39a02
Sebastian Slot Rohr <73996470+sebastianrohr@users.noreply.github.com>
2021-04-26 21:12:50 +0200

    Implemented two student queries

--- StudentModel.java
+++ StudentModel.java
@@ -8,7 +8,8 @@ public class StudentModel {
     Statement stmt = null;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
-    String selectedCourse = "Essential Computing 1 E2019";
+    String selectedCourse = "";
+    String selectedStudent = "";
 
     StudentModel(String url) {
         this.url = url;
@@ -27,8 +28,9 @@ public class StudentModel {
         this.stmt = conn.createStatement();
     }
 
-    public void CourseInfoQuery() {
-        String queryCourse = "SELECT *" +          //We find everything in the Students and courses tables. We then joins the student IDs and the course IDS with their respective tables from the Student_enrollment table.
+    public String CourseInfoQuery() {
+        String returnString = "";
+        String queryCourse = "SELECT *" + //We find everything in the Students and courses tables. We then joins the student IDs and the course IDS with their respective tables from the Student_enrollment table.
                 "FROM Student_enrollments " +
                 "JOIN Grades " +
                 "ON Student_enrollments.Grade_ID = Grades.Grade_ID " +
@@ -45,16 +47,21 @@ public class StudentModel {
                 String surname = rs.getString("surname");
                 String lastname = rs.getString("lastname");
                 int grade = rs.getInt("Grade");
-                System.out.println(surname + " " + lastname + " - Grade: " + grade);
+                returnString += surname + " " + lastname + " - Grade: " + grade + "\n";
             }
             rs = null;
+            return returnString;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        return returnString;
     }
-    public void AverageCourseGradeQuery() {
+
+
+    public String AverageCourseGradeQuery() {
         double avg = 0;
-        String queryCourse = "SELECT avg(grade)" +          //We find everything in the Students and courses tables. We then joins the student IDs and the course IDS with their respective tables from the Student_enrollment table.
+        String returnString = "";
+        String queryCourse = "SELECT avg(grade)" +          //We find everything in the Students and courses tables. We then join the student IDs and the course IDS with their respective tables from the Student_enrollment table.
                 "FROM Student_enrollments " +
                 "JOIN Grades " +
                 "ON Student_enrollments.Grade_ID = Grades.Grade_ID " +
@@ -68,10 +75,68 @@ public class StudentModel {
             while (rs != null && rs.next()) {
                 avg = rs.getDouble("avg(grade)");
             }
-            System.out.println("Average grade for " + selectedCourse + ": " + avg);
+            returnString = "Average grade: \n" + avg;
             rs = null;
+            return returnString;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return returnString;
+        }
+    }
+
+    public String AverageStudentGradeQuery() {
+        double avg = 0;
+        String returnString = "";
+        String queryStudentGrade = "SELECT avg(grade)" +          //We find everything in the Students and courses tables. We then joins the student IDs and the course IDS with their respective tables from the Student_enrollment table.
+                "FROM Student_enrollments " +
+                "JOIN Grades " +
+                "ON Student_enrollments.Grade_ID = Grades.Grade_ID " +
+                "JOIN Students " +
+                "ON Student_enrollments.Student_ID = Students.Student_ID " +
+                "WHERE Surname = '" + selectedStudent + "'" +
+                "or Lastname = '" + selectedStudent + "';";
+        try {
+            rs = stmt.executeQuery(queryStudentGrade);
+            if (rs == null)
+                System.out.println("No Student with that name");
+            while (rs != null && rs.next()) {
+                avg = rs.getDouble("avg(grade)");
+            }
+            returnString = "Average grade: " + avg;
+            rs = null;
+            return returnString;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return returnString;
+        }
+    }
+
+    public String StudentCoursesQuery() {
+        String returnString = "";
+        String queryStudentGrade = "SELECT Course_name, Grade " +          //We find everything in the Students and courses tables. We then joins the student IDs and the course IDS with their respective tables from the Student_enrollment table.
+                "FROM Student_enrollments " +
+                "JOIN Grades " +
+                "ON Student_enrollments.Grade_ID = Grades.Grade_ID " +
+                "JOIN Students " +
+                "ON Student_enrollments.Student_ID = Students.Student_ID " +
+                "JOIN Courses " +
+                "ON Student_enrollments.Course_ID = Courses.Course_ID " +
+                "WHERE Surname = '" + selectedStudent + "'" +
+                "or Lastname = '" + selectedStudent + "';";
+        try {
+            rs = stmt.executeQuery(queryStudentGrade);
+            if (rs == null)
+                System.out.println("No Student with that name");
+            while (rs != null && rs.next()) {
+                String course_name = rs.getString("Course_name");
+                int grade = rs.getInt("Grade");
+                returnString += course_name + " - Grade: " + grade + "\n";
+            }
+            rs = null;
+            return returnString;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        return returnString;
     }
 }
