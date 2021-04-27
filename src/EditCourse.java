ceac4299aa0b03df14512a57ae51ccc79ead0658
Max de Visser <m.madv96@gmail.com>
2021-04-27 15:04:43 +0200

    Update EditCourse.java

--- src/EditCourse.java
+++ src/EditCourse.java
@@ -1,18 +1,20 @@
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-public class EditCourse {
-    private static boolean result;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-    public static boolean display (){
+public class EditCourse {
+
+    public static void display(StudentModel SDB) {
+
         int windowWidth = 400;
         int windowHeight = 300;
 
@@ -22,33 +24,78 @@ public class EditCourse {
         window.setMinWidth(windowWidth);
         window.setMinHeight(windowHeight);
 
-        Label label = new Label("Edit the course");
-        ListView<String> studentList = new ListView<>(); //KOM HER TIL !!!!!!!!!!!!!!!!!!!
-        // Ovensåtende ListView skal være en liste af studerende på det kursus. Herefter skal man kunne vælge at give dem karktere
+
+        try {
+            SDB.connect();
+            SDB.createStatement();
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+        // -- COMBOBOX FOR GRADES
+        ComboBox<String> gradeCombobox = new ComboBox<>();
+        gradeCombobox.setPromptText("Choose Grade");
+        ArrayList<String> grades = SDB.getAllGrades();
+        gradeCombobox.getItems().addAll(grades); //Adds the different studentNames
+
+        gradeCombobox.setOnAction(event -> {
+            System.out.println("UPDATE GRADE HERE");
+        });
+
+        //STUDENT CURRENT GRADE LABEL
+        TextField currentGrade = new TextField();
+        currentGrade.setMaxWidth(100);
+        currentGrade.setEditable(false);
+
+        Label currentGradeText = new Label("Current grade:");
+
+        // -- LIST VIEW FOR STUDENTS
+        ListView<String> studentList = new ListView<>();
+        studentList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE); // can also be set to select multiple
+        studentList.setMinWidth(200);
+        ArrayList<String> studentNames = SDB.getStudentsInCourse();
+        studentList.getItems().addAll(studentNames); //Adds the different studentNames
+
+        studentList.getSelectionModel().selectedItemProperty().addListener((v, oldValue, newValue) -> { // adds a listener such that we can execute code when something is choosen in the list
+            SDB.selectedStudent = studentList.getSelectionModel().getSelectedItem();
+            try{
+                SDB.connect();
+                SDB.createStatement();
+                currentGrade.setText(SDB.studentCourseGradeQuery());
+                SDB.close();
+            } catch (SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+        });
+
 
         Button btnUpdateInformation = new Button("Update Information");
         btnUpdateInformation.setOnAction(e -> System.out.println("DO A UPDATE HERE"));
 
-        GridPane layout = new GridPane();
-        layout.setMinSize(windowWidth,windowHeight);
-        layout.setPadding(new Insets(10, 10, 10, 10));
-        layout.setVgap(5);
-        layout.setHgap(5);
-        layout.setAlignment(Pos.CENTER);
-        layout.add(label,0,0);
-        layout.add(btnUpdateInformation,1,0);
 
-        /*VBox layout = new VBox(10);
+        VBox layout1 = new VBox(20);
+        layout1.setPadding(new Insets(15, 12, 15, 12));
+        layout1.setSpacing(10);
+        layout1.getChildren().addAll(currentGradeText, currentGrade,gradeCombobox);
+
+
+        HBox layout = new HBox(10);
         layout.setPadding(new Insets(15, 12, 15, 12));
         layout.setSpacing(10);
-        layout.getChildren().addAll(label);
-        layout.setAlignment(Pos.CENTER);*/
+        layout.getChildren().addAll(studentList, layout1);
+        layout.setAlignment(Pos.CENTER);
+
+
+        try {
+            SDB.close();
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
 
         Scene scene = new Scene(layout);
         window.setScene(scene);
         window.showAndWait(); //is seen alot in conjunction with line 12, since it implies that we need to handle the window before going back
 
-        return result;
     }
 }
 
